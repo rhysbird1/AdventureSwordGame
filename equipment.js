8b5411fff68c82b307f95daff740b2eb6c8df0c3
rhysbird1 <32000278+rhysbird1@users.noreply.github.com>
2020-02-24 22:15:42 +0000

    0.1.10.2

--- equipment.js
+++ equipment.js
@@ -1,8 +1,8 @@
 var slot = {};
 
-var effect = {}
+var effect = {};
 
-var weapon = { name: ["Empty","Wooden Dagger", "Bronze Dagger", "Bronze Sword", "Iron Sword", 
+var weapon = { name: ["Empty", "Wooden Dagger", "Bronze Dagger", "Bronze Sword", "Iron Sword", 
                       "Steel Longsword", "Steel Claymore", "Greatsword", "Obisidian Dagger", 
                       "Obsidian Sword", "Glass WarAxe", "Crystal Dagger", "Crystal Greatsword"],
                level: 1,
@@ -20,7 +20,7 @@ var weapon = { name: ["Empty","Wooden Dagger", "Bronze Dagger", "Bronze Sword", 
                bonusAttackSpeed: 0,
                shardsNeededToUpgrade: 5,
                scrollsNeededToUpgrade: 1,
-               isOwned: true
+               isOwned: true,
              };
 
 var armour = { name: ["Empty", "Wooden Armour", "Rugged Leather", "Hard Leather", "Bronze Armour",
@@ -28,44 +28,64 @@ var armour = { name: ["Empty", "Wooden Armour", "Rugged Leather", "Hard Leather"
                level: 0,
                scroll: 0,
                heldScrolls: 0,
-               baseHealth: 0,
+               baseHealth: 250, //250 default
                bonusHealth: 0,
                baseDodgeChance: 2,
                bonusDodgeChance: 0,
-               shardsNeededToUpgrade: 2,
+               damageReduction: 0,
+               bonusDamageReduction: 0,
+               shardsNeededToUpgrade: 5,
+               scrollsNeededToUpgrade: 1,
+               isOwned: true,
+             };
+
+var head =   { name: ["Empty", "Leaf Cowl", "Cloth Headwrap", "Turtle Shell", "Wooden Helmet",
+                      "Plated Hood", "Spidersilk Hood", "Obisidian Helmet", "Glass Helmet",
+                      "Hood of Ghosts", "Hood of Saints", "Helmet of Gods", "Dragon-Leather Cowl"],
+               level: 0,
+               scroll: 0,
+               heldScrolls: 0,
+               baseHealth: 0,
+               damageReduction: 0,
+               bonusDamageReduction: 0,
+               shardsNeededToUpgrade: 5,
+               scrollsNeededToUpgrade: 1,
+               isOwned: true,
+             };
+
+var gloves = { name: ["Empty", "Worn Hand-Wraps", "Old Leather Gloves", "Wormsilk Gloves", 
+                      "Hard-Leather Gloves", "Spidersilk Gloves", "Golden Vambraces", 
+                      "Obisidian Gauntlets", "Spectral Gloves", "Gloves of Silence", 
+                      "[none]"],
+               level: 0,
+               scroll: 0,
+               heldScrolls: 0,
+               hitChance: 0.5,
+               shardsNeededToUpgrade: 5,
+               scrollsNeededToUpgrade: 1,
+               isOwned: true,
+             };
+
+var boots =  { name: ["Empty", "[none]", "[none]", "[none]", 
+                      "[none]", "[none]", "[none]", "[none]",
+                      "[none]", "[none]", "[none]"],
+               level: 0,
+               scroll: 0,
+               heldScrolls: 0,
+               dodgeChance: 1,
+               shardsNeededToUpgrade: 5,
                scrollsNeededToUpgrade: 1,
                isOwned: false,
              };
 
-var head =   { name: ["Empty", "Leaf Cowl", "Cloth Headwrap", "Turtle Shell", "Wooden Helmet",
-                      "Plated Hood", "Spidersilk Hood", "Obisidian Helmet", "Glass Helmet"
-                      ],
+var ring1 =  { name: ["Empty", "Cracked, Rusty Ring", "Cracked Blood Ring", "Blood Ring", 
+                      "Plated Blood Ring", "Enchanted Blood Ring", "Death-Imbued Blood Ring", 
+                      "Sacred Blood Ring", "Elder Blood Ring"],
                level: 0,
                scroll: 0,
                heldScrolls: 0,
-               isOwned: false,
-             };
-
-var gloves = { name: ["Empty"],
-               level: 0,
-               scroll: 0,
-               heldScrolls: 0,
-               hitChance: 0,
-               isOwned: false,
-             };
-
-var boots =  { name: ["Empty"],
-               level: 0,
-               scroll: 0,
-               heldScrolls: 0,
-               dodgeChance: 0,
-               isOwned: false,
-             };
-
-var ring1 =  { name: ["Empty"],
-               level: 0,
-               scroll: 0,
-               heldScrolls: 0,
+               shardsNeededToUpgrade: 7,
+               scrollsNeededToUpgrade: 2,
                isOwned: false,
              };
 
@@ -74,6 +94,8 @@ var ring2 =  { name: ["Empty"],
                scroll: 0,
                heldScrolls: 0,
                xpBonus: 0,
+               shardsNeededToUpgrade: 7,
+               scrollsNeededToUpgrade: 2,
                isOwned: false,
              };
 
@@ -81,6 +103,8 @@ var ring3 =  { name: ["Empty"],
                level: 0,
                scroll: 0,
                heldScrolls: 0,
+               shardsNeededToUpgrade: 7,
+               scrollsNeededToUpgrade: 2,
                isOwned: false,
              };
 
@@ -88,6 +112,8 @@ var ring4 =  { name: ["Empty"],
                level: 0,
                scroll: 0,
                heldScrolls: 0,
+               shardsNeededToUpgrade: 7,
+               scrollsNeededToUpgrade: 2,
                isOwned: false,
              };
 
@@ -95,8 +121,10 @@ var belt =   { name: ["Empty"],
                level: 0,
                scroll: 0,
                heldScrolls: 0,
-               critChance: 0,
-               critDamage: 0,
+               critChance: 1,
+               critDamage: 0.1,
+               shardsNeededToUpgrade: 10,
+               scrollsNeededToUpgrade: 3,
                isOwned: false,
              };
 
@@ -105,6 +133,8 @@ var pocket = { name: ["Empty"],
                scroll: 0,
                heldScrolls: 0,
                boneShardBonus: 0,
+               shardsNeededToUpgrade: 10,
+               scrollsNeededToUpgrade: 3,
                isOwned: false,
              };
 
@@ -112,13 +142,15 @@ var aura =   { name: ["Empty"],
                level: 0,
                scroll: 0,
                heldScrolls: 0,
+               healPercentOnDamage: 1,
+               shardsNeededToUpgrade: 15,
+               scrollsNeededToUpgrade: 5,
                isOwned: false,
              };
 
-
 function upgradeEquipment(item){
-    if (!item.isOwned) {
-        alert("You Do Not Own This Item");
+    if (!item.isOwned || isPlaying) {
+        alert("You can't do that right now.");
         return;
     } 
     
@@ -132,6 +164,7 @@ function upgradeEquipment(item){
         
     } else { 
         alert("Not enough resources");
+        return;
     }  
     
     upgradeStats(item);
@@ -141,17 +174,17 @@ function upgradeEquipment(item){
 function upgradeStats(item) {
     switch (item){
         case weapon:
-            weapon.baseDamage += (weapon.baseDamage / 2);
+            weapon.baseDamage += Math.round(weapon.baseDamage / 2);
             weapon.hitChance += 4;
             weapon.critDamage += 0.1;
             weapon.critChance += 1;
-            weapon.baseAttackSpeed -= 100;
+            weapon.baseAttackSpeed -= 75;
+            console.log("damage: " + weapon.baseDamage);
+            console.log("hit rate: " + weapon.hitChance);
+            break;
+        case armour:
+            armour.baseHealth += Math.ceil((armour.baseHealth / 3.3 + 1) / 10) * 10;
+            player.currentHealth = Math.round(armour.baseHealth + armour.bonusHealth + head.baseHealth);
             break;
     }
 }
-
-
-
-
-
-
